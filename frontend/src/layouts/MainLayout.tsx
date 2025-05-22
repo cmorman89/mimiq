c0a8d4ce00209ef84dfad445d64a6ddf593a3e48
Charles Morman <github@cmorman.com>
2025-05-22 12:53:20 -0700

    feat: responsive page container sizing allows scrolling on mobile

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -20,7 +20,7 @@ import { Navbar } from "../features/navbar/Navbar";
  */
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div id="main-layout" className="relative flex flex-col h-full w-full">
+    <div id="main-layout" className="relative flex flex-col min-h-full lg:h-full w-full">
       <Navbar />
       <main id="main-content" className="flex w-full h-full pb-6">
         {children}
