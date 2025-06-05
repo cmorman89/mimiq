34d4a82ffa3be9e1d1a8a40c4e45e91c2e4a6cfd
Charles Morman <github@cmorman.com>
2025-06-05 14:50:29 -0700

    feat: add custom hook for accessing BannerContext

---
+++ frontend/src/context/useBannerMessage.tsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { BannerContext } from "./banner_context/createBannerFormContext";
+
+export const useBannerContext = () => {
+  const context = useContext(BannerContext);
+  if (!context) {
+    throw new Error("useBannerMessage must be used within a BannerProvider");
+  }
+  return context;
+};
